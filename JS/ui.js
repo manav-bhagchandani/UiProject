e331e05c6000b86ce0ed168dcdb5430f5ddd2951
manav-bhagchandani <manav.bhagchandani@flipkart.com>
2023-01-24 12:20:34 +0530

    second commit

--- JS/ui.js
+++ JS/ui.js
@@ -1,4 +1,11 @@
 function clickHere(){
-    document.getElementById("flipkartLogoImg").style.display = "none";
-
+    var img = document.getElementById("flipkartLogoImg");
+    var para = document.getElementById("flipkartPara");
+      if (img.style.display === "none") {
+        img.style.display = "block";
+        para.style.display = "none";
+      } else {
+        img.style.display = "none";
+        para.style.display = "block";
+      }
 }
